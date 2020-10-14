91a31a32d6c2189b23a9c54e3a4cbad969ff2df7
Shivam <Shivam.Chandarana@softwire.com>
2020-10-14 13:53:10 +0100

    Account and Transaction Classes set up

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
 
 namespace SupportBank
 {
@@ -6,7 +8,56 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+    }
+
+    class Transaction
+    {
+        public Transaction(string date, string from, string to, string narrative, double amount)
+        {
+            Date = date;
+            From = from;
+            To = to;
+            Narrative = narrative;
+            Amount = amount;
+        }
+
+        public string Date { get;  }
+        public string From { get; }
+        public string To { get; }
+        public string Narrative { get; }
+        public double Amount { get; }
+    }
+
+    class Account
+    {
+        public Account(string name)
+        {
+            BalanceDue = 0.0;
+            Name = name;
+            Transactions = new List<Transaction>();
+
+        }
+        
+        public string Name { get; }
+        public double BalanceDue { get; set; }
+        public List<Transaction> Transactions { get; set; };
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+            if (transaction.To.Equals(Name))
+            {
+                BalanceDue += transaction.Amount;
+            }
+
+            if (transaction.From.Equals(Name))
+            {
+                BalanceDue -= transaction.Amount;
+            }
+        }
+
+
     }
 }
