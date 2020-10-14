ad7795c455be28476821e1fa66bb55f2f25d5ce8
Shivam <Shivam.Chandarana@softwire.com>
2020-10-14 16:36:56 +0100

    Forgot to add new class files oops

---
+++ SupportBank/Account.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SupportBank
+{
+    class Account
+    {
+        public string Name { get; }
+        public decimal BalanceDue { get; private set; }
+        public List<Transaction> Transactions { get; private set; }
+
+        public Account(string name)
+        {
+            BalanceDue = 0;
+            Name = name;
+            Transactions = new List<Transaction>();
+
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+            if (transaction.To.Equals(Name))
+            {
+                BalanceDue += transaction.Amount;
+            }
+
+            if (transaction.From.Equals(Name))
+            {
+                BalanceDue -= transaction.Amount;
+            }
+        }
+    }
+}
